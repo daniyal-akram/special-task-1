e37671523d0ebe6dffccbf2e9d810e1a43dd2fa9
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-14 16:43:20 +0500

    Add files via upload

---
+++ speacial task part 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{ float p,q,r,s;
+ cout<<"Enter the 1st Number=";
+ cin>>p;
+ cout<<"Enter the 2nd Number=";
+ cin>>q;
+ cout<<"Enter the 3rd Number=";
+ cin>>r;
+ cout<<"Enter the 4th Number=";
+ cin>>s;
+ cout<<"The Sum of Numbers="<<p+q+r+s<<endl;
+ cout<<"The product of Number="<<p*q*r*s<<endl;
+ cout<<"the Average of Number="<<(p+q+r+s)/4<<endl;
+}
